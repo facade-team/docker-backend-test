a8b681cdb255c33957052f496bcf85a75c2c818a
Seungwoo Kim <48646015+seungwooKim99@users.noreply.github.com>
2021-07-16 18:54:24 +0900

    2nd commit (#2)

--- src/index.py
+++ src/index.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!!!!!!!!!!'
 
 @app.route('/help')
 def help():
