97acf8856ae9e0e51207a6dfc83a5b9e902178d3
Seungwoo Kim <sonicdx886@gmail.com>
2021-07-17 17:42:25 +0900

    .

--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!!!!!!!!!! this is test'
+    return 'Hello, World!!!!!!!!!! this is test!!!'
 
 @app.route('/help')
 def help():
